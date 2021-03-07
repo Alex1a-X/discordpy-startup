82203c0a74aa2eaebe85f52cf636ee7783c4b4fd
Alex1a-X <70698950+Alex1a-X@users.noreply.github.com>
2021-03-07 23:18:39 +0900

    2020

--- discordbot.py
+++ discordbot.py
@@ -20,6 +20,9 @@ async def ping(ctx):
 @bot.command()
 async def info(ctx):
     await ctx.send('discordで主に活動している キチゲェw 紳士たちの集まりです。')
-
+    
+@bot.command()
+async def 2020(ctx):
+    await ctx.send('@everyone [サーバー更新のお知らせ]このサーバーに公式BOTができました！これから機能を追加するのでお待ちください！')
 
 bot.run(token)
